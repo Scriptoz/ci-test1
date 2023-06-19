b03014609c30717bb974e8e76d023a00c1d52d0d
Dmitriy Berezin <d.berezin@tencoins.org>
2023-06-19 18:25:24 +0300

    check size

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,15 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-contract-sizer";
 
-const config: HardhatUserConfig = {
+const config: any = {
   solidity: "0.8.18",
+
+  contractSizer: {
+    alphaSort: true,
+    runOnCompile: !!process.env.REPORT_CONTRACT_SIZE,
+    disambiguatePaths: false,
+  },
 };
 
 export default config;

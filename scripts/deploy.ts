f883a91ff3e7e3e30ac3504f2f46906fb549d28f
Dmitriy Berezin <d.berezin@tencoins.org>
2023-06-20 13:38:23 +0300

    declare config

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,22 +1,16 @@
-import { ethers } from "hardhat";
+import { ethers } from 'hardhat';
+import yaml from 'js-yaml';
+import fs from 'fs';
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const configPath = process.env.DEPLOY_CONFIG_PATH;
 
-  const lockedAmount = ethers.parseEther("0.001");
-
-  const lock = await ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  if (!configPath) {
+    throw new Error('"DEPLOY_CONFIG_PATH" env is undeclared');
+  }
+  
+  const config = yaml.load(fs.readFileSync(configPath, 'utf8'));
+  console.log('config', config);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
